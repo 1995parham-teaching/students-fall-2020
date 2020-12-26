7fccb9a24276f2e2c1a56fd00f74650856610f60
Parham Alvani <parham.alvani@gmail.com>
2020-12-26 10:59:58 +0330

    feat: Create Gorm Connection

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/labstack/echo/v4 v4.1.17
-	gorm.io/driver/sqlite v1.1.4 // indirect
-	gorm.io/gorm v1.20.9 // indirect
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.20.9
 )
--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/aut-ce/students.go/handler"
+	"github.com/aut-ce/students.go/model"
+	"github.com/aut-ce/students.go/store"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 	e := echo.New()
 
+	db, err := gorm.Open(sqlite.Open("students.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// disabling gorm logs
+	// db.Logger.LogMode(logger.Silent)
+
+	if err := db.Migrator().AutoMigrate(&model.Student{}); err != nil {
+		log.Fatal(err)
+	}
+
 	e.GET("/hello", handler.Hello)
 
-	s := handler.Student{}
+	s := handler.NewStudent(store.NewSQLStudent(db))
 	e.POST("/student", s.Create)
+	e.GET("/student", s.Find)
 
 	if err := e.Start("0.0.0.0:8080"); err != nil {
 		fmt.Println(err)
